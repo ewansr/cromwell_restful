80da60a76b503b8cdba695538f2f08302bc5a293
Saúl Euán <ewansr@gmail.com>
2016-09-29 04:27:28 -0500

    login con pass

--- controllers/usuarios.php
+++ controllers/usuarios.php
@@ -33,10 +33,9 @@ class usuarios extends master_usuarios{
         // TODO: Implement delete() method.
     }
 
-    private function autenticar($correo, $contrasena)
-    {
+    private function autenticar($correo, $contrasena) {
         $comando  = "SELECT u.*, ".
-            " AES_DECRYPT(contrasena,'AES2016') as pass_decrypt ".
+            " AES_DECRYPT(u.contrasena,'AES2016') as pass_decrypt ".
             " FROM " .
             self::TABLE_NAME.
             " as u WHERE u." . self::USUARIO . "=?";
@@ -46,13 +45,11 @@ class usuarios extends master_usuarios{
             $sentencia = Conexion::obtenerInstancia()->obtenerdb()->prepare($comando);
 
             $sentencia->bindParam(1, $correo);
-
             $sentencia->execute();
 
             if ($sentencia) {
                 $resultado = $sentencia->fetch();
-
-                if (true==true/*self::validarContrasena($contrasena, $resultado['contrasena'])*/) {
+                if ($contrasena === $resultado['pass_decrypt']) {
                     return true;
                 } else return false;
             } else {
@@ -81,78 +78,33 @@ class usuarios extends master_usuarios{
                 return null;
     }
 
-    private function login()
-    {
-
+    private function login() {
         $body = file_get_contents('php://input');
         $respuesta = array();
         $usuario = json_decode($body);
-
-        //var_dump($usuario);
-
         $correo     = $usuario->correo;
         $contrasena = $usuario->contrasena;
-
 
         if (self::autenticar($correo, $contrasena)) {
             try {
                 $usuarioBD = self::obtenerUsuarioPorCorreo($correo);
                 if ($usuarioBD != NULL) {
                     http_response_code(200);
+                    $respuesta[self::ID] = $usuarioBD[self::ID];
                     $respuesta[self::USUARIO] = $usuarioBD[self::USUARIO];
-                    return ["estado" => 1, "correo" => $correo, "usuario" => $respuesta];
+                    return ["logueo_valido" => 1,
+                            "valid" => 1,
+                            "message" => "Logueo Exitoso",
+                            self::TABLE_NAME => $respuesta];
                 } else {
-                    throw new ApiExceptions(self::ESTADO_FALLA_DESCONOCIDA,
-                        "Ha ocurrido un error vvv");
+                    return ["logueo_valido" => 0, "message" => "Usuario o contraseña inválidos"];
                 }
             }catch (Exception $e){
                 throw new ApiExceptions(self::ESTADO_FALLA_DESCONOCIDA,
                     "Ha ocurrido un error vvv" . $e->getMessage() );
             }
         } else {
-            throw new ApiExceptions(self::ESTADO_PARAMETROS_INCORRECTOS,
-                utf8_encode("Correo o contraseña inválidos"));
+            return ["logueo_valido" => 0, "message" => "Usuario o contraseña inválidos"];
         }
     }
-    
-//    public function login(){
-////        $SQLCommand = "SELECT " .self::ID.",".self::IDPERSONAL.",".self::USUARIO." FROM " . self::TABLE_NAME ;
-//        $SQLCommand = "SELECT *, ".
-//            " AES_DECRYPT(contrasena,'AES2016') as pass_decrypt".
-//            " FROM " .
-//            self::TABLE_NAME;
-//
-//        self::$sentence = conexion::obtenerInstancia()->obtenerDB()->prepare($SQLCommand);
-//
-//        if (self::$sentence->execute()) {
-//            $data = self::$sentence->fetch(PDO::FETCH_ASSOC);
-//            if ($data != NULL) {
-//                http_response_code(200);
-//                $row = $data;
-////                $jsonData = array();
-////                while ($row = mysql_fetch_row(self::$sentence)) {
-//
-//                    $response[self::ID] = $row[self::ID];
-//                    $response[self::IDPERSONAL] = $row[self::IDPERSONAL];
-//                    $response[self::USUARIO] = $row[self::USUARIO];
-//                    $response[self::CONTRASENA_DECRYPT] = $row[self::CONTRASENA_DECRYPT];
-//                    $response[self::COMENTARIOS] = $row[self::COMENTARIOS];
-//                    $response[self::EDITABLE] = $row[self::EDITABLE];
-//                    $response[self::IDPERFIL] = $row[self::IDPERFIL];
-//                    $response[self::ACTIVO] = $row[self::ACTIVO];
-////                    $jsonData = $row;
-////                }
-//                    return [
-//                        "Found" => true,
-//                        SELF::TABLE_NAME => $response,
-//                        "SQL" => self::$sentence
-//                    ];
-//
-//            } else {
-//                return ["Found" => False, "usuario" => "Usuario no encontrado"];
-//                throw new ExcepcionApi(self::ESTADO_FALLA_DESCONOCIDA,
-//                    "Ha ocurrido un error");
-//            }
-//        }
-//    }
 }
